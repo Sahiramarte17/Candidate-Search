5fad641516e4ad20e3c27bed98a628c8b1965542
Sahiramarte17 <sahiramarte17@gmail.com>
2024-10-09 19:59:30 -0400

    Updating nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Candidate Search</Link>
+        </li>
+        <li>
+          <Link to="/saved">Saved Candidates</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
