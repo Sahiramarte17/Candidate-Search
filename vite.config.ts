9d742f602b72d7c77c8037c5ddf05439b35bcbbe
Sahiramarte17 <sahiramarte17@gmail.com>
2024-10-14 11:52:24 -0400

    updated vite.config for deployement to render

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  /*
   server: {
     port: 3000,
     open: true,
   },
+  */
 });
